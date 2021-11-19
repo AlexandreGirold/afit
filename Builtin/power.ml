0732e331180650e3d56baf11358b53643c51c452
DieuCrevette <92374888+DieuCrevette@users.noreply.github.com>
2021-11-19 13:01:45 +0100

    update power

--- Builtin/power.ml
+++ Builtin/power.ml
@@ -7,13 +7,20 @@ open Basic_arithmetics
     @param x base
     @param n exponent
  *)
-let pow x n = 0
+let pow x n = let rec aux x n = match n with
+                |0->1
+                |n-> x*(aux x (n-1) )
+              in aux x n;;
+
 
 (** Fast integer exponentiation function. Logarithmic complexity.
     @param x base
     @param n exponent
  *)
-let power x n = 0
+let rec power x n = if (x=1)||(n=0) then 1
+                    else if n mod 2 = 0 then power x (n/2)*power x (n/2)
+                    else power x (n-1)*x;;
+
 
 (** Fast modular exponentiation function. Logarithmic complexity.
     @param x base
