3d34593be7e71f6096d11aa802694b7a97eb18c8
DieuCrevette <92374888+DieuCrevette@users.noreply.github.com>
2021-11-22 21:06:35 +0100

    Update test_primes.ml

--- Builtin/test_primes.ml
+++ Builtin/test_primes.ml
@@ -5,8 +5,13 @@ open Basic_arithmetics
 open Power
 
 (** Deterministic primality test *)
-let is_prime n = true
 
+let is_prime n = let rec prime n i =
+                   if n mod i =0 then false
+                   else if (i*i)>n then true
+                   else prime n (i+1)
+                 in prime n 2;;
+                 
 (** Primality test based on small Fermat theorem
     @param p tested integer
     @param testSeq sequence of integers against which to test
